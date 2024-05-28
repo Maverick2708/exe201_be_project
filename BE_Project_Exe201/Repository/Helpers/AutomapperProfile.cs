a3fc52e968506b2026501c8a83d4d3080e74f394
Maverick2708 <daigiakien02@gmail.com>
2024-05-28 15:53:20 +0700

    #Commit Add automapper

---
+++ BE_Project_Exe201/Repository/Helpers/AutomapperProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Helpers
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            
+        }
+    }
+}
